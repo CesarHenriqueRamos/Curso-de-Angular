af0af33f324252c1bed279ed00adb98654363eca
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-10-19 17:30:00 -0300

    fim do modulo

---
+++ pj-pipes/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+import { FiltroArrayPipe } from './filtro-array.pipe';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExemplosPipesComponent,
+    CamelCasePipe,
+    FiltroArrayPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    /*SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps:[SettingsService],
+      useFactory: (settingsService) => settingsService.getLocale()
+    }*/
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ pj-pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { rejects } from 'assert';
+import { resolve } from 'dns';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent implements OnInit {
+  livro:any ={
+    title: 'Javascript para web',
+    rating: 4.5678,
+    numeroPaginas: 345,
+    preco: 50,
+    dataLancamento: new Date(2016, 5, 13)
+  }
+  livros:string[] = ['agular 2', 'react'];
+  valorAsync = new Promise((resolve , reject)=>{
+    setTimeout(()=> resolve('Valor asincrono'), 2000);
+  });
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
