2994766c518fd709ecb46016e96f11e94fe53d41
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-10-26 18:50:35 -0300

    fim das atividades do dia 26-10-2020

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-template-form',
@@ -8,13 +10,27 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
   usuario:any = {
     nome: null,
-    email: null
+    email: null,
   }
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
   }
   onSubmit(form){
-    console.log(this.usuario);
+    console.log(form.value);
+  }
+  consutaCep(cep){
+    //limpar cep
+    cep = cep.replace(/\D/g , '');
+    if(cep != ""){
+      //função validar cep
+      var cepValido = /^[0-9]{8}$/
+      if(cepValido.test(cep)){
+        this.httpClient.get("https://viacep.com.br/ws/"+ cep +"/json/").subscribe(dados =>{
+          console.log(dados);
+        })
+        
+      }
+    }
   }
 }
