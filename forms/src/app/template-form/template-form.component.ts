8a7daaf1e698aa701d851008971a1ff6251c1456
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-10-27 15:15:23 -0300

    fim do modulo template-form

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -17,20 +17,54 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit(): void {
   }
   onSubmit(form){
-    console.log(form.value);
+    //console.log(form.value);
+    //enviando  dados para o formulario
+    this.httpClient.post('enderecoServidor/userForm', JSON.stringify(form.value)).subscribe(dados => console.log(dados));
   }
-  consutaCep(cep){
+  consutaCep(cep, form){
     //limpar cep
     cep = cep.replace(/\D/g , '');
     if(cep != ""){
       //função validar cep
       var cepValido = /^[0-9]{8}$/
+      this.reseteFormulario(form);
       if(cepValido.test(cep)){
         this.httpClient.get("https://viacep.com.br/ws/"+ cep +"/json/").subscribe(dados =>{
-          console.log(dados);
+          this.populaDadosForm(dados,form);
         })
         
       }
     }
+  }
+  populaDadosForm(dados, form){
+    //metudo setValue
+    /*form.setValue({
+      bairro: dados.bairro,
+      cep: dados.cep,
+      cidade: dados.localidade,
+      complemento: dados.complemento,
+      email: form.value.email,
+      estado: dados.uf,
+      nome: form.value.nome,
+      numero: "",
+      rua: dados.logradouro,
+    })*/
+    //metudo patchValue
+    form.form.patchValue({
+      estado: dados.uf,
+      rua: dados.logradouro,
+      cidade: dados.localidade,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+    })
+  }
+  reseteFormulario(form){
+    form.form.patchValue({
+      estado: null,
+      rua: null,
+      cidade: null,
+      complemento: null,
+      bairro: null,
+    })
   }
 }
